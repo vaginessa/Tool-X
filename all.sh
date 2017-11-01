71eb3ba5901b2b8e934ac4bfa6998d85eb7dffb3
Rajkumrdusad <32831684+Rajkumrdusad@users.noreply.github.com>
2017-11-01 12:51:57 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,7 +1,5 @@
 cd
 apt-get update
 apt-get upgrade
-apt install nano
-&& pkg install nano
-apt install python
-&& pkg install python
+apt install nano && pkg install nano
+apt install python && pkg install python
