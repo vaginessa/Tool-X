b0eb55d88bc983673d2710af4421c7ec7dca832e
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2018-09-27 19:18:33 +0530

    Tool-X V_2.0

---
+++ .install.py
@@ -0,0 +1,404 @@
+# Tool Name :- Tool-X
+# Author :- Rajkumar dusad
+# Date :- 1/11/2017
+# Powered By :- Aex Software's
+
+import os
+import sys
+from time import sleep
+
+if os.path.exists("/data/data/com.termux/files/usr/bin/pkg"):
+    systm="termux"
+    hpath=os.getenv("HOME")+"/"
+    bpath="/data/data/com.termux/files/usr/bin/"
+elif os.path.exists("/usr/bin/apt"):
+    if os.path.exists("/usr/lib/sudo"):
+        systm="ubuntu"
+        bpath="/usr/bin/"
+        hpath=os.getenv("HOME")+"/"
+    else:
+        systm="debian"
+        bpath="/usr/bin/"
+        hpath="/home/"
+elif os.path.exists("/bin/apt"):
+    if os.path.exists("/usr/lib/sudo"):
+        systm="ubuntu"
+        bpath="/usr/bin/"
+        hpath=os.getenv("HOME")+"/"
+    else:
+        systm="debian"
+        bpath="/usr/bin/"
+        hpath=os.getenv("HOME")+"/"
+
+if os.path.exists("/data/data/com.termux/files/usr/bin/pkg"):
+    pac="pkg"
+elif os.path.exists("/usr/bin/apt"):
+    if os.path.exists("/usr/lib/sudo"):
+        pac="sudo apt-get"
+    else:
+        pac="apt-get"
+elif os.path.exists("/bin/apt"):
+    if os.path.exists("/usr/lib/sudo"):
+        pac="sudo apt-get"
+    else:
+        pac="apt-get"
+
+def Aex():
+	Aex=sys.exit()
+
+def Ux():
+	Ux=os.system("clear")
+
+def Inst():
+    if systm=="termux":
+        print("\033[1;36m\007")
+        sys.stdout.write(open(".Toolo2/.T0.aex").read())
+        print("\033[1;33m")
+        sys.stdout.write(open(".Toolo2/.Ix.aex").read())
+        print("\033[1;36m")
+        sys.stdout.write(open(".Toolo2/.T0.aex").read())
+        print("\033[00m")
+
+    else:
+        print("\033[1;36m")
+        sys.stdout.write(open(".Toolo2/.T0u.aex").read())
+        print("\033[1;33m")
+        sys.stdout.write(open(".Toolo2/.Ixu.aex").read())
+        print("\033[1;36m")
+        sys.stdout.write(open(".Toolo2/.T0u.aex").read())
+        print("\033[00m")
+
+def Nerr():
+  if systm=="termux":
+	Ux()
+	print ('''
+
+
+\033[1;33m
+          _____           _    __  __
+         |_   _|__   ___ | |   \ \/ /
+           | |/ _ \ / _ \| |____\  /
+           | | (_) | (_) | |____/  \     
+           |_|\___/ \___/|_|   /_/\_\ \033[1;m\033[1;91mv2.0\033[1;m
+
+
+\033[1;36m ===============================================\033[1;m
+\033[1;33m|           Install Best Hacking Tool           |
+\033[1;36m ===============================================\033[1;m
+
+\033[1;31m  [ + ]  \033[1;31mWe can't install Tool-X.\033[1;m
+\033[1;31m  [ + ]  \033[1;31mThere are some error.\033[1;m
+\033[1;31m  [ + ]  \033[1;31mPlease try again after some time.\033[1;m
+\033[1;36m_________________________________________________
+=================================================\033[1;m
+''')
+  else:
+	Ux()
+	print ('''
+
+
+\033[1;33m
+               _____           _    __  __
+              |_   _|__   ___ | |   \ \/ /
+                | |/ _ \ / _ \| |____\  /
+                | | (_) | (_) | |____/  \     
+                |_|\___/ \___/|_|   /_/\_\ \033[1;31mv2.0
+\033[1;m
+
+\033[1;36m =========================================================\033[1;m
+\033[1;33m|\033[1;m               \033[1;33m Install Best Hacking Tool.\033[1;m               \033[1;33m|\033[1;m
+\033[1;36m =========================================================\033[1;m
+
+\033[1;31m  [ + ]  \033[1;31mWe can't install Tool-X.\033[1;m
+\033[1;31m  [ + ]  \033[1;31mThere are some error.\033[1;m
+\033[1;31m  [ + ]  \033[1;31mPlease try again after some time.\033[1;m
+\033[1;36m___________________________________________________________
+===========================================================\033[1;m
+''')
+
+def Logo():
+    if systm=="termux":
+        print ('''\007\033[1;33m
+
+
+          _____           _    __  __
+         |_   _|__   ___ | |   \ \/ /
+           | |/ _ \ / _ \| |____\  /
+           | | (_) | (_) | |____/  \     
+           |_|\___/ \___/|_|   /_/\_\ \033[1;m\033[1;91mv2.0
+
+
+\033[1;36m ===============================================
+\033[1;33m|           \033[1;31m Notic for You from Tool-X.         \033[1;33m|
+\033[1;36m ===============================================
+
+\033[1;33m  [ + ] \033[1;32mUse It At Your Own Risk.
+\033[1;33m  [ + ] \033[1;32mNo Warranty.
+\033[1;33m  [ + ] \033[1;32mUse it legal purpose only.
+\033[1;33m  [ + ] \033[1;32mWe are not responsible for your actions.
+\033[1;33m  [ + ] \033[1;32mDo not do things that are forbidden.
+
+\033[1;31m If you are installing this tool.
+ that means you are agree with all terms.
+\033[1;36m_________________________________________________
+\033[1;36m=================================================\033[00m
+			''')
+
+    else:
+        print ('''
+
+
+\033[1;33m
+               _____           _    __  __
+              |_   _|__   ___ | |   \ \/ /
+                | |/ _ \ / _ \| |____\  /
+                | | (_) | (_) | |____/  \     
+                |_|\___/ \___/|_|   /_/\_\ \033[1;31mv2.0
+
+\033[1;36m ==========================================================
+\033[1;33m|                \033[1;32m Notic for You from Tool-X.               \033[1;33m|
+\033[1;36m ==========================================================
+
+\033[1;33m   [ + ] \033[1;32mUse It At Your Own Risk.
+\033[1;33m   [ + ] \033[1;32mNo Warranty.
+\033[1;33m   [ + ] \033[1;32mUse it legal purpose only.
+\033[1;33m   [ + ] \033[1;32mWe are not responsible for your actions.
+\033[1;33m   [ + ] \033[1;32mDo not do things that are forbidden.
+
+\033[1;31m  If you are installing this tool.
+  that means you are agree with all terms.
+\033[1;36m___________________________________________________________
+===========================================================\033[00m
+			''')
+
+
+def Sc():
+    if systm=="termux":
+        print ('''\007\033[1;33m
+
+
+          _____           _    __  __
+         |_   _|__   ___ | |   \ \/ /
+           | |/ _ \ / _ \| |____\  /
+           | | (_) | (_) | |____/  \     
+           |_|\___/ \___/|_|   /_/\_\ \033[1;m\033[1;91mv2.0
+
+
+\033[1;36m ===============================================
+\033[1;33m|           Install Best Hacking Tool           |
+\033[1;36m ===============================================
+
+\033[1;33m    [ + ] \033[1;32mTool-X installed successfully.
+\033[1;33m    [ + ] \033[1;32mTo run Tool-X.
+\033[1;33m    [ + ] \033[1;32mType Tool-X in your terminal.
+\033[1;36m_________________________________________________
+=================================================\033[00m
+''')
+    else:
+        print ('''
+
+
+\033[1;33m
+               _____           _    __  __
+              |_   _|__   ___ | |   \ \/ /
+                | |/ _ \ / _ \| |____\  /
+                | | (_) | (_) | |____/  \     
+                |_|\___/ \___/|_|   /_/\_\ \033[1;31mv2.0
+
+\033[1;36m =========================================================
+\033[1;33m|               \033[1;33m Install Best Hacking Tool.               \033[1;33m|
+\033[1;36m =========================================================
+
+\033[1;33m     [ + ] \033[1;32mTool-X installed successfully.
+\033[1;33m     [ + ] \033[1;32mTo run Tool-X.
+\033[1;33m     [ + ] \033[1;32mType Tool-X in your terminal.
+\033[1;36m___________________________________________________________
+===========================================================\033[00m
+''')
+
+
+def req():
+    if systm=="ternux":
+        if os.path.exists("/data/data/com.termux/files/usr/bin/python"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python")
+            os.system(pac+" install -y python-pip")
+
+        if os.path.exists("/data/data/com.termux/files/usr/bin/python2"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python2")
+            os.system(pac+" install -y python2-pip")
+
+        if os.path.exists("/data/data/com.termux/files/usr/bin/git"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y git")
+
+    elif systm=="ubuntu":
+        if os.path.exists("/usr/bin/python"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python")
+            os.system(pac+" install -y python-pip")
+
+        if os.path.exists("/usr/bin/python2"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python2")
+            os.system(pac+" install -y python2-pip")
+
+        if os.path.exists("/usr/bin/git"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y git")
+
+    elif systm=="debian":
+        if os.path.exists("/usr/bin/python"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python")
+            os.system(pac+" install -y python-pip")
+
+        if os.path.exists("/usr/bin/python2"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y python2")
+            os.system(pac+" install -y python2-pip")
+
+        if os.path.exists("/usr/bin/git"):
+            print ("\033[1;33m Done....\033[1;m")
+        else:
+            os.system(pac+" update")
+            os.system(pac+" -y upgrade")
+            os.system(pac+" install -y git")
+
+def delok():
+    Ux()
+    if systm=="ubuntu":
+        os.system("cd "+hpath+".Tool-X && sudo rm -rf install.aex README.md .install.aex .U.aex .sc")
+        Ux()
+    else:
+        os.system("cd "+hpath+".Tool-X && rm -rf install.aex README.md .install.aex .U.aex .sc")
+        Ux()
+    Sc()
+
+def delf():
+  if os.path.exists(hpath+".Tool-X"):
+    delok()
+  else:
+    Nerr()
+
+def insok():
+  if os.path.exists(bpath+"Tool-X"):
+    delf()
+  else:
+    Nerr()
+
+def proce():
+	Inst()
+	print ('''\n\033[1;32mInstalling.........\033[1;m''')
+	req()
+	print ('''\033[1;32m Installing.........\033[1;m''')
+	if systm=="ubuntu":
+	    os.system("cd "+hpath+" && sudo rm -rf .Tool-X")
+	    os.system("cd "+bpath+" && sudo rm -rf Tool-X")
+	    os.system("sudo mkdir "+hpath+".Tool-X")
+	    os.system("sudo mv -v .1 .sc .Toolo .Toolo2 .Xecn "+hpath+".Tool-X")
+	    os.system("cd .. && sudo rm -rf Tool-X")
+	    os.system("cd .. && sudo rm -rf Tool-X")
+	    os.system("cd "+hpath+".Tool-X && sudo mv .1 .tools")
+	    os.system("cd "+hpath+".Tool-X && sudo mv -v .tools "+hpath+".Tool-X/.Xecn")
+	    os.system("cd "+hpath+".Tool-X && sudo mv -v .Toolo "+hpath+".Tool-X/.Xecn/.tools")
+	    os.system("cd "+hpath+".Tool-X/.Xecn/ && sudo mkdir .sc")
+	    os.system("cd "+hpath+".Tool-X && sudo mv .Toolo2 .Toolo")
+	    os.system("cd "+hpath+".Tool-X && sudo mv -v .Toolo "+hpath+".Tool-X/.Xecn/.sc/")
+	    os.system("cd "+hpath+".Tool-X/.Xecn/.tools && sudo cp Tool-X "+bpath)
+	    os.system("cd "+hpath+".Tool-X/.Xecn/.tools && sudo rm -rf Tool-X")
+
+	    print ('''\033[1;32mGiving permissiom level for enabling Tool-X execute from terminal\033[1;m''')
+	    os.system("cd "+bpath+" && sudo chmod +x Tool-X")
+
+	    print ('''\033[1;32mGiving permission to Tool-X directory\033[1;m''')
+	    os.system("cd "+hpath+" && sudo chmod +x .Tool-X")
+
+	    print ('''\033[1;32mGiving permission to files in Tool-X directory\033[1;m''')
+	    os.system("cd "+hpath+".Tool-X/.Xecn/ && sudo chmod +x *.* .* .*.* *")
+	    Ux()
+	    insok()
+
+	else:
+	    os.system("cd "+hpath+" && rm -rf .Tool-X")
+	    os.system("cd "+bpath+" && rm -rf Tool-X")
+	    os.system("mkdir "+hpath+".Tool-X")
+	    os.system("mv -v .1 .sc .Toolo .Toolo2 .Xecn "+hpath+".Tool-X")
+	    os.system("cd .. && rm -rf Tool-X")
+	    os.system("cd .. && rm -rf Tool-X")
+	    os.system("cd "+hpath+".Tool-X && mv .1 .tools")
+	    os.system("cd "+hpath+".Tool-X && mv -v .tools "+hpath+".Tool-X/.Xecn")
+	    os.system("cd "+hpath+".Tool-X && mv -v .Toolo "+hpath+".Tool-X/.Xecn/.tools")
+	    os.system("cd "+hpath+".Tool-X/.Xecn/ && mkdir .sc")
+	    os.system("cd "+hpath+".Tool-X && mv .Toolo2 .Toolo")
+	    os.system("cd "+hpath+".Tool-X && mv -v .Toolo "+hpath+".Tool-X/.Xecn/.sc/")
+	    os.system("cd "+hpath+".Tool-X/.Xecn/.tools && cp Tool-X "+bpath)
+	    os.system("cd "+hpath+".Tool-X/.Xecn/.tools && rm -rf Tool-X")
+
+	    print ('''\033[1;32mGiving permissiom level for enabling Tool-X execute from terminal\033[1;m''')
+	    os.system("cd "+bpath+" && chmod 777 Tool-X")
+	    print ('''\033[1;32mGiving permission to Tool-X directory\033[1;m''')
+	    os.system("cd "+hpath+" && chmod +x .Tool-X")
+
+	    print ('''\033[1;32mGiving permission to files in Tool-X directory\033[1;m''')
+	    os.system("cd "+hpath+".Tool-X/.Xecn/ && chmod +x *.* .*.* * .*")
+	    Ux()
+	    insok()
+
+
+
+def Toolx():
+		Ux()
+		Logo()
+		Tool = raw_input('''\033[1;33m Do you want to install Tool-X [Y/n]> \033[00m''')
+			
+		while Tool == "Y" or Tool == "y":
+			Ux()
+			proce()
+			Toolo = raw_input("\033[1;33m Press any key to continue >>> \033[00m")
+			if Toolo == "":
+			   Aex()
+			else:
+			   Aex()
+			
+		while Tool == "n" or Tool == "N":
+			Aex()
+			
+		else:
+			Toolx()
+
+
+
+def Tool_X():
+	try:
+		Toolx()
+
+	except KeyboardInterrupt:
+		Ux()
+		Aex()
+Tool_X()
