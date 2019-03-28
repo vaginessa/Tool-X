d2f19e977f15f6f5582aff51ede6924b1315ebe6
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2019-03-28 20:13:48 +0530

    Update back function.

--- modules/cat10.py
+++ modules/cat10.py
@@ -60,7 +60,7 @@ def cat10():
 		usr.write("217")
 		usr.close()
 		inst1()
-	elif Toolo == "00":
+	elif Toolo == "00" or Toolo=="back":
 		pass
 	else:
 		print ("\033[01;34m\007\n error : \033[01;37m\'"+Toolo+"\' \033[01;31minvalid option !!!")
